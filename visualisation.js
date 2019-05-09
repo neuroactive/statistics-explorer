4f49f0174ea81051bf7cea706a125abd38ca2b72
Chris Thompson <info@speaklanguages.com>
2019-05-09 23:17:21 +0300

    Change position of tooltip

--- visualisation.js
+++ visualisation.js
@@ -88,7 +88,7 @@ function ready(error, data) {
             });
             tooltip.classed('hidden', false)
                 .attr('style', 'left:' + (mouse[0]) +
-                        'px; top:' + (mouse[1] + 40) + 'px')
+                        'px; top:' + (mouse[1] + 30) + 'px')
                 .html(d.properties.MNIMI);
         })
         .on('mouseout', function() {
